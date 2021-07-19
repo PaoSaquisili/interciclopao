b517bdf1445946ce1096a120b29cfdfc85444e7b
PaoSaquisili <87342520+PaoSaquisili@users.noreply.github.com>
2021-07-18 23:16:38 -0500

    inicial
    inicial

---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using ApiInterciclo.Entities;
+using ApiInterciclo.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiInterciclo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public readonly Datacontext _datacontext;
+        public UserController(Datacontext datacontext)
+        {
+            _datacontext = datacontext;
+        }
+
+        [HttpGet("auth/{username}/{password}")]
+        public async Task<ActionResult> authAsync(string username, string password)
+        {
+            User usario = await _datacontext.User.FirstOrDefaultAsync(x => x.username == username && x.password == password);
+            if (usario != null)
+            {
+                return Ok(usario);
+            }
+            else
+            {
+                return BadRequest("Error en autentificar");
+            }
+        }
+
+
+        [HttpGet("alluser")]
+        public async Task<ActionResult> AllUser()
+        {
+            List<User> usario = await _datacontext.User.ToListAsync();
+            if (usario != null)
+            {
+                return Ok(usario);
+            }
+            else
+            {
+                return BadRequest("Error en autentificar");
+            }
+        }
+    }
+}
---
+++ Helpers/Datacontext.cs
@@ -0,0 +1,18 @@
+﻿using ApiInterciclo.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiInterciclo.Helpers
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> option) : base(option)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
